c2b1839784b8409f5de2e0287fd51f53105e3b86
guidovdijk <funnydutchguy@gmail.com>
2022-10-13 14:40:27 +0200

    Initial Commit

---
+++ student648539/student648539App.swift
@@ -0,0 +1,17 @@
+//
+//  student648539App.swift
+//  student648539
+//
+//  Created by Guido van Dijk on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct student648539App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
